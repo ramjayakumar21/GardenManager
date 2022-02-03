5ef74c990ea0a2ddae622850d147aea2c43a3f08
ramjayakumar21 <66887504+rameyland@users.noreply.github.com>
2022-02-02 23:26:08 -0800

    started specification of classes

---
+++ src/main/model/GardenApp.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.HashSet;
+
+/*
+Class representing Garden with multiple plants
+ */
+public class GardenApp {
+    private HashSet<Plant> gardenPlants;
+
+    public GardenApp() {
+        // stub
+    }
+
+    public HashSet<Plant> getGardenPlants() {
+        return gardenPlants;
+    }
+}
---
+++ src/main/model/Plant.java
@@ -0,0 +1,19 @@
+package model;
+
+/*
+Represents plant and given attributes
+*/
+public class Plant {
+
+    public Plant() {
+        // stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: If isDry = true, change to false and return isDry
+    // else return isDry
+    public void water() {
+        // stub
+    }
+
+}
